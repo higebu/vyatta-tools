2bcdb9cb6899c6d32dafaa6f68924d7b280f2298
higebu <yuya.kusakabe@gmail.com>
2012-05-27 16:35:18 +0900

    Refactoring

--- show-dhcp-shared-network.pl
+++ show-dhcp-shared-network.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 # show-dhcp-shared-network.pl
 # Output dhcp-server's shared-network-name in JSON format
 #
@@ -14,12 +14,6 @@ my $level = "service dhcp-server shared-network-name";
 $config->setLevel($level);
 my @pools = $config->listOrigNodes();
 
-my %server_hash = ();
-my @server_list = ();
-my %map_hash = ();
-my %cidr_hash = ();
-my @cidr_list = ();
-my %subnet_hash = ();
 my @pool_list = ();
 my %pool_hash = ();
 my @shared_network_list = ();
@@ -28,24 +22,30 @@ my %shared_network_hash = ();
 foreach my $pool (@pools) {
   $config->setLevel("$level $pool subnet");
   my @cidrs = $config->listOrigNodes();
+  my %map_hash = ();
+  my %cidr_hash = ();
+  my @cidr_list = ();
+  my %subnet_hash = ();
   foreach my $cidr (@cidrs) {
     $config->setLevel("$level $pool subnet $cidr static-mapping");
     my @maps = $config->listOrigNodes();
+    my %server_hash = ();
+    my @server_list = ();
     foreach my $map (@maps) {
       my $ip = $config->returnOrigValue("$map ip-address");
       my $mac = $config->returnOrigValue("$map mac-address");
       $server_hash{$map}{'ip-address'} = $ip;
       $server_hash{$map}{'mac-address'} = $mac;
-      push(@server_list, { %server_hash });
     }
+    push(@server_list, { %server_hash });
     $map_hash{'static-mapping'} = [ @server_list ];
     $cidr_hash{$cidr} = { %map_hash };
-    push(@cidr_list, { %cidr_hash });
   }
+  push(@cidr_list, { %cidr_hash });
   $subnet_hash{'subnet'} = [ @cidr_list ];
   $pool_hash{$pool} = { %subnet_hash };
-  push(@shared_network_list, { %pool_hash });
 }
+push(@shared_network_list, { %pool_hash });
 $shared_network_hash{'shared-network-name'} = [ @shared_network_list ];
 
 my $ref = \%shared_network_hash;
